d8e95361d9fefcb074deec79f97d220b0034cbd5
Dinesh Borude <79857147+dineshborude@users.noreply.github.com>
2024-02-20 20:54:37 +0530

    Find Needle In Object Array

---
+++ src/main/java/com/collections/practise/FindNeedleInTrash.java
@@ -0,0 +1,18 @@
+package com.collections.practise;
+
+public class FindNeedleInTrash {
+
+    public static void main(String[] args) {
+
+        Object[] arr = {"hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"};
+        int size = arr.length;
+
+
+
+
+
+
+    }
+
+
+}
