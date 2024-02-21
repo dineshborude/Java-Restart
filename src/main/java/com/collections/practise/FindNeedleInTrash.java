649a3fc9c8cfa095812aa175ae6ada56cafbfe9f
Dinesh Borude <79857147+dineshborude@users.noreply.github.com>
2024-02-21 12:05:33 +0530

    Found Needle

--- src/main/java/com/collections/practise/FindNeedleInTrash.java
+++ src/main/java/com/collections/practise/FindNeedleInTrash.java
@@ -7,6 +7,18 @@ public class FindNeedleInTrash {
         Object[] arr = {"hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"};
         int size = arr.length;
 
+        for(int i = 0; i < size; i++) {
+
+            if(arr[i] != null && arr[i].equals("needle")) {
+
+                System.out.println("Found Needle at " +i);
+
+            }
+            else {
+                System.out.println("NOT FOUND");
+            }
+
+        }
 
 
 
