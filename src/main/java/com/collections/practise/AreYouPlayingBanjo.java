fff1d886bb220323d87993442d9962a866afd3b6
Dinesh Borude <79857147+dineshborude@users.noreply.github.com>
2024-02-21 15:27:31 +0530

    Banjo Played

---
+++ src/main/java/com/collections/practise/AreYouPlayingBanjo.java
@@ -0,0 +1,34 @@
+package com.collections.practise;
+
+public class AreYouPlayingBanjo {
+
+    public static String banjoBajao(String name) {
+        char[] startswith = name.toCharArray();
+        if (startswith[0] == 'R' || startswith[0] == 'r') {
+            return name + " plays banjo";
+        } else {
+            return name + " does not play banjo";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(banjoBajao("itika"));
+    }
+}
+
+
+    /*
+    Create a function which answers the question "Are you playing banjo?".
+    If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+    The function takes a name as its only argument, and returns one of the following strings:
+
+    name + " plays banjo"
+    name + " does not play banjo"
+    Names given are always valid strings.
+     */
+
+
+
+
+
